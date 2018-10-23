56f6b12fc17bb07419533a8866cd3efdd4dbdb63
Karim Yarboua <karim.yarboua@open-groupe.com>
2018-10-23 19:57:33 +0200

    BLE -- Remove testing tracker because unused

--- app/src/main/java/com/telen/ble/blemanagersample/ApplicationComponent.java
+++ app/src/main/java/com/telen/ble/blemanagersample/ApplicationComponent.java
@@ -1,8 +1,7 @@
 package com.telen.ble.blemanagersample;
 
 import com.telen.ble.blemanagersample.devices.minger.Minger_P50;
-import com.telen.ble.blemanagersample.devices.oc100.OC100;
-import com.telen.ble.manager.di.LibraryComponent;
+import com.telen.ble.sdk.di.LibraryComponent;
 
 import dagger.Component;
 
@@ -11,5 +10,4 @@ import dagger.Component;
 })
 public interface ApplicationComponent {
     void inject(Minger_P50 target);
-    void inject(OC100 target);
 }
