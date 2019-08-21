f3ba3b487646281dab1e0a359b6c5f57e00c49d3
Karim Yarboua <karim.yarboua@gmail.com>
2019-08-21 23:02:03 +0200

    Protocols -- Handle UndeliverableExceptions from rx polideo library

--- ble-protocols/src/main/java/com/telen/sdk/ble/di/BleManager.java
+++ ble-protocols/src/main/java/com/telen/sdk/ble/di/BleManager.java
@@ -2,7 +2,11 @@ package com.telen.sdk.ble.di;
 
 import android.content.Context;
 
+import com.polidea.rxandroidble2.exceptions.BleException;
 import com.telen.sdk.common.di.CommonProtocolsManager;
+
+import io.reactivex.exceptions.UndeliverableException;
+import io.reactivex.plugins.RxJavaPlugins;
 
 public class BleManager {
 
@@ -20,5 +24,13 @@ public class BleManager {
                 .builder()
                 .commonLibraryComponent(CommonProtocolsManager.getInstance(context))
                 .build();
+
+        RxJavaPlugins.setErrorHandler(throwable -> {
+            if (throwable instanceof UndeliverableException && throwable.getCause() instanceof BleException) {
+                return; // ignore BleExceptions as they were surely delivered at least once
+            }
+            // add other custom handlers if needed
+            throw new RuntimeException("Unexpected Throwable in RxJavaPlugins error handler", throwable);
+        });
     }
 }
